1b90bfe933d0615f43252a1ab6a97b9bd7884b24
Mranxiaoranran <2767855941@qq.com>
2019-07-03 20:39:52 +0800

    spring-cloud-gateway 裸奔版本

---
+++ spring-cloud-sofa-gate/src/main/java/sofa/gate/GateApplication.java
@@ -0,0 +1,19 @@
+package sofa.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateApplication.class, args);
+    }
+
+
+
+}
