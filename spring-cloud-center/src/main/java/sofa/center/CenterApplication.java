01e191c7c09b41d9dc38ceee7f4ea285d258a513
Mranxiaoranran <2767855941@qq.com>
2019-07-03 13:11:49 +0800

    裸奔版 注册中心

---
+++ spring-cloud-center/src/main/java/sofa/center/CenterApplication.java
@@ -0,0 +1,14 @@
+package sofa.center;
+@EnableEurekaServer
+@SpringBootApplication
+public class CenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenterApplication.class, args);
+    }
+
+
+
+}
+
+
