dfe6751057f4ddca93f035084a6b948b953ff6d4
Mranxiaoranran <2767855941@qq.com>
2019-07-03 13:38:33 +0800

    裸奔版 注册中心

--- spring-cloud-center/src/main/java/sofa/center/CenterApplication.java
+++ spring-cloud-center/src/main/java/sofa/center/CenterApplication.java
@@ -1,4 +1,9 @@
 package sofa.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class CenterApplication {
